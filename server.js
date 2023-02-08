2c26b00b4b6dfe5f0072a1826ba4c911e164b96e
Daniel De La Pena <dadelapema@gmail.com>
2023-02-07 19:56:11 -0700

    connection to db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+require("dotenv").config();
+const db = require("./db/connection")
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+ })

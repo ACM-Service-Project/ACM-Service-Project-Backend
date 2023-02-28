c55b7c2784c231a43a74595aaf80ace9e3774c0c
Daniel De La Pena <del17011@byui.edu>
2023-02-27 21:08:57 -0700

    added user controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,4 +11,56 @@ const getUsers = async (req, res, next) => {
   });
 };
 
-module.exports = { getUsers };
+const getUserById = (req, res, next) => {
+  UserModel.findById(req.params.id, (err, doc) => {
+    if (err) next(new Error400(err.message));
+    else res.status(200).send(doc);
+  });
+};
+
+const addUser = (req, res, next) => {
+  UserModel.create(req.body, (err, doc) => {
+    if (err) next(new Error400(err.message));
+    else res.status(201).send(doc);
+  });
+};
+
+const editUser = (req, res, next) => {
+  UserModel.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
+    if (err) next(new Error400(err.message));
+    else if (doc) {
+      res.status(200).send(doc);
+    }
+  });
+};
+
+const deleteUser = (req, res, next) => {
+  UserModel.findByIdAndDelete(req.params.id, (err, doc) => {
+    if (err) next(new Error400(err.message));
+    else if (doc) {
+      res.status(204);
+    }
+  });
+};
+
+const setAccountStatus = (req, res, next) => {
+  UserModel.findByIdAndUpdate(
+    req.params.id,
+    { active: req.params.status },
+    (err, doc) => {
+      if (err) next(new Error400(err.message));
+      else if (doc) {
+        res.status(200).send(doc);
+      }
+    }
+  );
+};
+
+module.exports = {
+  getUsers,
+  addUser,
+  editUser,
+  getUserById,
+  deleteUser,
+  setAccountStatus,
+};

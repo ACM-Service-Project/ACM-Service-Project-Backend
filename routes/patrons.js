54e53eeb8d42e39095ee35e3fa95fb705862bb27
Ben Davis <bdavis493@byui.edu>
2023-03-09 15:19:27 -0700

    Wrote controller functions for patrons

---
+++ routes/patrons.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const PatronsController = require("../controllers/patronsController");
+
+router.get('/getPatronById/:patronId',PatronsController.getPatronById);
+router.get('/getAllPatrons',PatronsController.getAllPatrons);
+router.post('/searchPatrons',PatronsController.searchPatrons);
+router.post('/addPatron',PatronsController.addPatron);
+router.patch('/updatePatron/:patronId',PatronsController.updatePatron);
+router.delete('/deletePatron/:patronId',PatronsController.deletePatron);
+
+module.exports = router;
